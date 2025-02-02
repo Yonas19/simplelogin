b0c78194ed8b45ddba7089e3d25befd76df3edd4
Yonas19 <yonjr0936@gmail.com>
2025-02-02 15:03:11 +0400

    finished up setting up login handle function

--- loginSignup/src/Registration/Login.jsx
+++ loginSignup/src/Registration/Login.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { supabase } from "../Client";
 
-function Login({change}) {
+function Login({ change }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const { data, error } = await supabase.auth.SignInWithPassword({
+      email,
+      password,
+    });
+  };
   return (
     <div>
       <form>
@@ -8,8 +18,18 @@ function Login({change}) {
           <div className="title-container">
             <h2 className="title-name">Login</h2>
             <div className="input-container">
-              <input type="text" placeholder="username" />
-              <input type="password" placeholder="password" />
+              <input
+                type="text"
+                placeholder="username"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
               <button>Login</button>
               <p onClick={change}>Don't have account</p>
             </div>
