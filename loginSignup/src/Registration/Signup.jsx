68b64944e1515f8658138df2963477a62c0327c5
Yonas19 <yonjr0936@gmail.com>
2025-02-02 14:46:34 +0400

    Signup page is now functional

--- loginSignup/src/Registration/Signup.jsx
+++ loginSignup/src/Registration/Signup.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { supabase } from "../Client";
 
-function Signup({ change}) {
+function Signup({ change }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    const { data, error } = await supabase.auth.signUp({ email, password });
+    if (error) setError(error.message);
+    else {
+      alert("Successgully registered");
+      change();
+    }
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={handleSignUp}>
         <div className="form-container">
           <div className="title-container">
             <h2 className="title-name">Signup</h2>
-            <div className="input-container">
-              <input type="email" placeholder="email" />
-              <input type="text" placeholder="username" />
-              <input type="password" placeholder="password" />
+            {error && <p style={{ color: "red" }}>{error}</p>}
 
-              <button>Signup</button>
+            <div className="input-container">
+              <input type="email" placeholder="email" value = {email} onChange={(e) => setEmail(e.target.value)} />
+              <input type="text" placeholder="username" />
+              <input type="password" placeholder="password" value={password} onChange ={(e) => setPassword(e.target.value)} />
+
+              <button type="submit">Sign Up</button>
               <p onClick={change}>Already have an account</p>
             </div>
           </div>
