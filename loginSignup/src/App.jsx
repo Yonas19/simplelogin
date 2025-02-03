2a62c5da97394d504c901ae4c5e3ce9822bbcb39
Yonas19 <yonjr0936@gmail.com>
2025-02-03 23:29:07 +0400

    added the user check or getuser functionality using useEFfect

--- loginSignup/src/App.jsx
+++ loginSignup/src/App.jsx
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Signup from "./Registration/Signup";
 import Login from "./Registration/Login";
+import { supabase } from "./Client";
 
 function App() {
   const [signedUp, setSignedUp] = useState(true);
+  const [user, setUser] = useState(null)
   const Change = () => {
     setSignedUp(!signedUp);
   };
-  
-
-
+  useEffect(() => {
+    const getUser = async () => {
+      const { data: { user } } = await supabase.auth.getUser()
+      setUser(user)
+    } 
+    getUser()
+  },[])
   return (
     <div style={{ marginTop: 50 }}>
       {signedUp ? <Login change={Change} /> : <Signup change={ Change} />}</div>
